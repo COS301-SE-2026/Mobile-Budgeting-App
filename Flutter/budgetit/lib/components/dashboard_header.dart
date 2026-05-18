bda82bbbf36ebb651111c9f0f86bd0acfdbb2de6
JessicaM-coder <jessicam33327@gmail.com>
2026-05-18 18:30:15 +0200

    addition on header

--- Flutter/budgetit/lib/components/dashboard_header.dart
+++ Flutter/budgetit/lib/components/dashboard_header.dart
@@ -2,3 +2,77 @@ import 'package:flutter/material.dart';
 
 class DashboardHeader extends StatelessWidget {
   const DashboardHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Padding(
+
+      padding: EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 15,
+      ),
+
+      child: Row(
+
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+        children: [
+
+          // burger menu
+          Container(
+
+            padding: EdgeInsets.all(10),
+
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+            ),
+
+            child: Icon(
+              Icons.menu,
+              size: 28,
+            ),
+          ),
+
+          // month selector
+          Container(
+
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 12,
+            ),
+
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(20),
+            ),
+
+            child: Row(
+              children: [
+                //i need to make a drop down for date
+                Text(
+                  "MAY 2026",
+
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                SizedBox(width: 10),
+
+                Icon(Icons.keyboard_arrow_down),
+
+              ],
+            ),
+          ),
+
+          // profile/settings icon
+         
+
+        ],
+      ),
+    );
+  }
+}
