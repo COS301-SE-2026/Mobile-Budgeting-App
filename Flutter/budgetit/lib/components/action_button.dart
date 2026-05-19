01d2e41bef2bf06a777574a210e41b04d74620a0
JessicaM-coder <jessicam33327@gmail.com>
2026-05-19 08:43:34 +0200

    action button

--- Flutter/budgetit/lib/components/action_button.dart
+++ Flutter/budgetit/lib/components/action_button.dart
@@ -11,36 +11,64 @@ class ActionButton extends StatelessWidget {
     required this.label,
   });
 
+  // theme colors
+  static const Color darkGreen = Color(0xFF04240C);
+  static const Color cream = Color(0xFFDDD6AE);
+  static const Color teal = Color(0xFF137E84);
+
   @override
   Widget build(BuildContext context) {
 
     return Container(
 
       width: 90,
-      padding: EdgeInsets.symmetric(
+
+      padding: const EdgeInsets.symmetric(
         vertical: 15,
       ),
 
       decoration: BoxDecoration(
-        color: Colors.white,
+
+        color: darkGreen,
+
         borderRadius: BorderRadius.circular(20),
+
+        border: Border.all(
+          color: cream.withOpacity(0.25),
+          width: 1,
+        ),
+
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.25),
+            blurRadius: 8,
+            offset: const Offset(0, 4),
+          ),
+        ],
       ),
 
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+
         children: [
 
           Icon(
             icon,
             size: 30,
-            color: Colors.black,
+            color: cream,
           ),
 
-          SizedBox(height: 10),
+          const SizedBox(height: 10),
 
           Text(
             label,
-            style: TextStyle(
+
+            textAlign: TextAlign.center,
+
+            style: const TextStyle(
+              color: cream,
               fontWeight: FontWeight.bold,
+              fontSize: 13,
             ),
           ),
 
