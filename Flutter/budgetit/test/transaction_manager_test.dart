8ea227660f0416071c1f1a20f6c231d32229f4c2
Do-nny <DonovanNelson05@gmail.com>
2026-06-03 18:33:25 +0200

    Fix: Unused import

--- Flutter/budgetit/test/transaction_manager_test.dart
+++ Flutter/budgetit/test/transaction_manager_test.dart
@@ -1,4 +1,3 @@
-import 'package:budgetit/database/app_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:budgetit/shared/widgets/badge.dart';
