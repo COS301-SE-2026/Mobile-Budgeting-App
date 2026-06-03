58da624de9c80656a2c92f95efb86670a02943b0
Do-nny <DonovanNelson05@gmail.com>
2026-06-03 17:01:53 +0200

    Bug : commas instead of semi-colons
    Chore: dart format

--- Flutter/budgetit/test/support/mock_db.dart
+++ Flutter/budgetit/test/support/mock_db.dart
@@ -10,36 +10,34 @@ import '../database/mock_daos.mocks.dart';
 class MockDb {
   MockDb()
     : db = MockAppDatabase(),
-      transactionDao = MockTransactionDao() ,
-      categoryDao = MockCategoryDao() ,
-      budgetDao = MockBudgetDao() ,
-      settingsDao = MockSettingsDao(){
-        when(db.transactionDao).thenReturn(transactionDao),
-        when (db.categoryDao).thenReturn(categoryDao) ,
-        when (db.budgetDao).thenReturn(budgetDao),
-        when (db.settingsDao).thenReturn(settingsDao);
-      }
+      transactionDao = MockTransactionDao(),
+      categoryDao = MockCategoryDao(),
+      budgetDao = MockBudgetDao(),
+      settingsDao = MockSettingsDao() {
+    when(db.transactionDao).thenReturn(transactionDao);
+    when(db.categoryDao).thenReturn(categoryDao);
+    when(db.budgetDao).thenReturn(budgetDao);
+    when(db.settingsDao).thenReturn(settingsDao);
+  }
 
-      final MockAppDatabase db ;
-      final MockTransactionDao transactionDao ;
-      final MockCategoryDao categoryDao ;
-      final MockBudgetDao budgetDao ;
-      final MockSettingsDao settingsDao ;
+  final MockAppDatabase db;
+  final MockTransactionDao transactionDao;
+  final MockCategoryDao categoryDao;
+  final MockBudgetDao budgetDao;
+  final MockSettingsDao settingsDao;
 }
 
-Widget wrapWithProviders(Widget child , {required AppDatabase db}) =>
-  MultiProvider(
-    providers: [
-      Provider<AppDatabase>.value(value: db),
-      ChangeNotifierProvider(create: (_) => ThemeProvider()) ,
-    ],
-    child: MaterialApp(home: child),
-  );
+Widget wrapWithProviders(Widget child, {required AppDatabase db}) =>
+    MultiProvider(
+      providers: [
+        Provider<AppDatabase>.value(value: db),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+      ],
+      child: MaterialApp(home: child),
+    );
 
-Widget wrapWithTheme(Widget child , {bool wrapInScaffold = true}) =>
-  ChangeNotifierProvider(
-    create: (_) => ThemeProvider(),
-    child: MaterialApp(
-      home: wrapInScaffold ? Scaffold(body:child) : child ,
-    ),
-  );
+Widget wrapWithTheme(Widget child, {bool wrapInScaffold = true}) =>
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: MaterialApp(home: wrapInScaffold ? Scaffold(body: child) : child),
+    );
