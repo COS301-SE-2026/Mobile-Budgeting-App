98c5637bb854db732761dd12301a213705ebe58b
ok-kiolin <u22647300@tuks.co.za>
2026-07-02 13:56:30 +0200

    fix: correct ImportResult class definition and method name; add csv and crypto dependencies

--- Flutter/budgetit/lib/models/import/import_result.dart
+++ Flutter/budgetit/lib/models/import/import_result.dart
@@ -4,19 +4,18 @@
 /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\\/\/\/\/\/\/ 
 */
 
-class ImportResult { final int totalPRarsed; final int inserted;  final int duplicatesSkipped;  final int failed;   final Map<String, String> errors;
+class ImportResult { final int totalParsed; final int inserted;  final int duplicatesSkipped;  final int failed;   final Map<String, String> errors;
 
-    const ImportedResult({
-        required this.totalParsed, required this.inserted, required this,duplicatesSkipped, required this.failed, this.errors = const{},  
+    const ImportResult({
+        required this.totalParsed, required this.inserted, required this.duplicatesSkipped, required this.failed, this.errors = const{},  
     });
 
     bool get isFullSucess => failed == 0 && duplicatesSkipped == 0;
     bool get hasInserts => inserted > 0;
 
     @override
-    Strign toString() =>
+    String toString() =>
     'ImportResult(parsed: $totalParsed, inserted: $inserted,'
     'duplicates: $duplicatesSkipped, failed: $failed)';
-
     
 }
