39bda6a605b4c6be43339b5bb29fd5cdebefae85
JessicaM-coder <jessicam33327@gmail.com>
2026-07-03 10:19:31 +0200

    footer

--- Flutter/budgetit/lib/components/landingpage/footer.dart
+++ Flutter/budgetit/lib/components/landingpage/footer.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import '../../utils/app_colour.dart';
 
 class Footer extends StatelessWidget {
-  const Footer({super.key});
+  final colours = MyColours();
+  Footer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    return Container(
+      width: double.infinity,
+      color: colours.background,
+      padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+      child: Column(
+        children: [
+          
+          Text(
+            "© 2026 Dev Oops. All rights reserved.",
+            style: TextStyle(
+              color: colours.whiteAccents,
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FooterLink extends StatelessWidget {
+  final String label;
+  const _FooterLink(this.label);
+
+  @override
+  Widget build(BuildContext context) {
+    final colours = MyColours();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: TextButton(
+        onPressed: () {},
+        child: Text(
+          label,
+          style: TextStyle(color: colours.whiteAccents),
+        ),
+      ),
+    );
   }
 }
