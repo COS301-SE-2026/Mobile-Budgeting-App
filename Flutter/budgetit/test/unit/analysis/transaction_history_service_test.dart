9fa2e6e5e28e9856b55f6cd208abf9668c66c531
Do-nny <DonovanNelson05@gmail.com>
2026-07-28 08:05:22 +0200

    Fix : Incorrect import paths in transaction_history_service_test.dart (typos)
    1. annotation -> annotations
    2. mokito -> mockito
    3. service -> services

--- Flutter/budgetit/test/unit/analysis/transaction_history_service_test.dart
+++ Flutter/budgetit/test/unit/analysis/transaction_history_service_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotation.dart';
-import 'package:mockito/mokito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:budgetit/database/app_database.dart';
 import 'package:budgetit/database/daos/transaction_dao.dart';
 import 'package:budgetit/database/daos/category_dao.dart';
-import 'package:budgetit/service/analysis/transaction_history_service.dart';
+import 'package:budgetit/services/analysis/transaction_history_service.dart';
 
 @GenerateMocks([AppDatabase, TransactionDao,CategoryDao])
 import 'transaction_history_service_test.mocks.dart';
