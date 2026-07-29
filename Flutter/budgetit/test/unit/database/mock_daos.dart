2dcc7bd36387d67d48f1d287a25ad01525392d94
Do-nny <DonovanNelson05@gmail.com>
2026-07-29 21:06:28 +0200

    test: Add mock recurring transaction DAO

--- Flutter/budgetit/test/unit/database/mock_daos.dart
+++ Flutter/budgetit/test/unit/database/mock_daos.dart
@@ -3,11 +3,13 @@ import 'package:budgetit/database/daos/budget_dao.dart';
 import 'package:budgetit/database/daos/category_dao.dart';
 import 'package:budgetit/database/daos/settings_dao.dart';
 import 'package:budgetit/database/daos/transaction_dao.dart';
+import 'package:budgetit/database/daos/recurring_transaction_dao.dart';
 import 'package:budgetit/database/app_database.dart';
 
 @GenerateNiceMocks([
   MockSpec<CategoryDao>(),
   MockSpec<TransactionDao>(),
+  MockSpec<RecurringTransactionDao>(),
   MockSpec<BudgetDao>(),
   MockSpec<SettingsDao>(),
   MockSpec<AppDatabase>(),
