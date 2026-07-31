4fca2c41a69cb44357f9449fa315c436a333b5e2
ok-kiolin <u22647300@tuks.co.za>
2026-07-31 02:37:24 +0200

    restructure Transaction Dialog thing, pending

--- Flutter/budgetit/lib/shared/widgets/add_edit_recurring_transaction_dialog.dart
+++ Flutter/budgetit/lib/shared/widgets/add_edit_recurring_transaction_dialog.dart
@@ -5,3 +5,39 @@ import 'package:decimal/decimal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+
+
+class AddEditRecurringTransactionDialog extends StatefulWidget {
+
+  final RecurringTransaction? existing;
+  final VoidCallback? onSaved;
+  final VoidCallback? onDeleted;
+  const AddEditRecurringTransactionDialog({
+    super.key,
+    this.existing,
+    this.onSaved,
+    this.onDeleted,
+  });
+
+  @override
+  State<AddEditRecurringTransactionDialog> createState() => _AddEditRecurringTransactionDialogState();
+}
+
+
+class _AddEditRecurringTransactionDialogState extends State<AddEditRecurringTransactionDialog> {
+  final _formKey = GlobalKey<FormState>();
+  late final TextEditingController _descController;
+  late final TextEditingController _amountController;
+  late TransactionType _type;
+  late DateTime _startDate;
+  late PeriodType _unit;
+  late int _intervalAmount;
+  bool _saving = false;
+
+  bool get _isEditing => widget.existing != null;
+
+  static const _months = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+  ];
+}
