e90cddf7530c7da0e4aece0e6403ce8602654bec
LufunoMphagi <lufunomphagi@gmail.com>
2026-08-06 12:14:15 +0200

    Add platform file export saver

---
+++ Flutter/budgetit/lib/services/file_export_saver_stub.dart
@@ -0,0 +1,7 @@
+Future<void> saveAndOpenFile({
+  required List<int> bytes,
+  required String fileName,
+  required String mimeType,
+}) {
+  throw UnsupportedError('Local file saving is not supported on this platform.');
+}
