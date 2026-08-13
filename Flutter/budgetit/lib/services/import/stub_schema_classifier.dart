50defb798033f7da7acb0c6a58b93762c8c942dc
ok-kiolin <u22647300@tuks.co.za>
2026-08-13 20:01:42 +0200

    added stub schema classifier

---
+++ Flutter/budgetit/lib/services/import/stub_schema_classifier.dart
@@ -0,0 +1,24 @@
+import '../../models/import/candidate_row.dart';
+import '../../models/import/statement_schema.dart';
+import 'schema_discovery_service.dart';
+
+class StubSchemaClassifier implements SchemaClassifier {
+  @override
+  Future<StatementSchema> classify(List<CandidateRow> sampleRows) async {
+    final markers = sampleRows.map((r) => r.signMarker?.toUpperCase()).where((m) => m != null).toSet();
+
+    if (markers.contains('CR') && !markers.contains('-')) {
+      return const StatementSchema(
+        signConvention: SignConvention.crSuffixMeansIncome,
+      );
+    }
+
+    if (markers.contains('-') && !markers.contains('CR')) {
+      return const StatementSchema(
+        signConvention: SignConvention.minusPrefixMeansExpense,
+      );
+    }
+
+    return const StatementSchema(signConvention: SignConvention.keywordBased);
+  }
+}
