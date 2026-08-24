c0d4111b89a2e762733339bc5fc0fc6166de3f61
PavthePekka <pavannaidoo6@gmail.com>
2026-08-24 12:28:18 +0200

    changed daos to make them consistent with schema. NOTE I COMMITED THE BUILD FILES,THESE ARE GENERATED BY DART NOT ME, i added them for the sake of brevity

--- Flutter/budgetit/lib/database/daos/category_dao.dart
+++ Flutter/budgetit/lib/database/daos/category_dao.dart
@@ -83,6 +83,7 @@ class CategoryDao extends DatabaseAccessor<AppDatabase>
       await into(categories).insert(companion);
       await into(categoryClosure).insert(
         CategoryClosureCompanion.insert(
+          id: _uuid.v4(),
           ancestorId: id,
           descendantId: id,
           depth: 0,
