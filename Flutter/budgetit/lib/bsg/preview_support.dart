662aec0503ad8b7b00d78e592903a81cc108224a
JessicaM-coder <jessicam33327@gmail.com>
2026-09-03 23:13:06 +0200

    fix app preview

--- Flutter/budgetit/lib/bsg/preview_support.dart
+++ Flutter/budgetit/lib/bsg/preview_support.dart
@@ -3,18 +3,28 @@ import 'package:budgetit/utils/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-Widget appPreview({required Widget child, bool database = false}) {
+Widget appPreview({
+  required Widget child,
+  bool database = false,
+  AppDatabase? appDatabase,
+}) {
   return Builder(
     builder: (context) {
       final isDark = Theme.of(context).brightness == Brightness.dark;
 
+      assert(
+        !database || appDatabase != null,
+        'An AppDatabase must be provided when database is true.',
+      );
+
       return MultiProvider(
         providers: [
-          ChangeNotifierProvider(create: (_) => ThemeProvider(isDark: isDark)),
-          if (database)
-            Provider<AppDatabase>(
-              create: (_) => AppDatabase(),
-              dispose: (_, value) => value.close(),
+          ChangeNotifierProvider(
+            create: (_) => ThemeProvider(isDark: isDark),
+          ),
+          if (database && appDatabase != null)
+            Provider<AppDatabase>.value(
+              value: appDatabase,
             ),
         ],
         child: child,
