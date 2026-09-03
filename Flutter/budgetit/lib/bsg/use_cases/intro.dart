c345e8593d3948840446487d9ce0aa8f5f4df1cf
JessicaM-coder <jessicam33327@gmail.com>
2026-09-03 07:16:03 +0200

    transaction card and dialoag design fix

--- Flutter/budgetit/lib/bsg/use_cases/intro.dart
+++ Flutter/budgetit/lib/bsg/use_cases/intro.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/material.dart';
-
 import 'package:budgetit/utils/app_colour.dart';
+import 'package:flutter/material.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 class Intro {}
